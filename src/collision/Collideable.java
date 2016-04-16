594616aef43e8209a2666f196491ea61c9d97591
fotoply <norberg.niels@gmail.com>
2016-04-16 16:40:00 +0200

    Add collideable interface

---
+++ src/collision/Collideable.java
@@ -0,0 +1,11 @@
+package collision;
+
+/**
+ * Created 4/16/16
+ *
+ * @author Niels Norberg
+ */
+public interface Collideable {
+    public boolean collidesWith(Collideable otherCollideable);
+
+}
